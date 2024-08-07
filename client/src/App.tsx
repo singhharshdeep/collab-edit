c1d30011b255451594c9dccda3f212e9d9c0f3dc
Harshdeep Singh <singh.harshdeep@hotmail.com>
2024-08-06 21:38:36 -0400

    Socket io init

---
+++ client/src/App.tsx
@@ -0,0 +1,28 @@
+import { useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import Editor from "./editor";
+import Header from "./header";
+
+function App() {
+  const editor = useEditor({
+    extensions: [
+      StarterKit.configure({
+        heading: {
+          levels: [1, 2, 3, 4, 5, 6],
+        },
+      }),
+    ],
+    content: "",
+  });
+
+  if (!editor) return null;
+
+  return (
+    <div className="bg-indigo-100">
+      <Header editor={editor} />
+      <Editor editor={editor} />
+    </div>
+  );
+}
+
+export default App;
