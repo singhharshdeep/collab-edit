85b6986f90e2df35297f1bd06c929ebb601dc456
Harshdeep Singh <harshdeepsingh@Harshdeeps-MacBook-Pro.local>
2024-08-07 22:25:54 -0400

    Socket io basic functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,49 @@
+import { useEffect, useState } from "react";
 import { useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+
+import { socket } from "./socket";
 import Editor from "./editor";
 import Header from "./header";
 
 function App() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
   const editor = useEditor({
     extensions: [
-      StarterKit.configure({
-        heading: {
-          levels: [1, 2, 3, 4, 5, 6],
-        },
-      }),
+      StarterKit.configure(),
     ],
     content: "",
   });
 
+  editor?.on("update", ({ editor }) => {
+    socket.emit("send-update", editor.getHTML());
+    console.log(editor.getHTML());
+  });
+
+  socket.on("receive-update", (data) => {
+    editor?.commands.setContent(data);
+  });
+
   if (!editor) return null;
+
+  useEffect(() => {
+    function onConnect() {
+      console.log("Connected to the server");
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+    };
+  }, []);
 
   return (
     <div className="bg-indigo-100">
---
+++ server/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+
+const corsOptions = {
+    origin: "*",
+    optionsSuccessStatus: 200
+  };
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+    }
+});
+
+app.use(cors(corsOptions));
+
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on("send-update", (data) => {
+        console.log("Doc updated");
+        console.log(data);
+        socket.broadcast.emit("receive-update", data);
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
